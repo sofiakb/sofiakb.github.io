d2370bb19b4821cee6b1307156c99e1591cbcbfa
Sofiakb <contact.sofiakb@gmail.com>
2022-04-02 17:20:10 +0200

    Updated: Vue pwa

---
+++ li-berty/precache-manifest.21dcdfd44745245c6bca3ac5dc906351.js
@@ -0,0 +1,334 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "108c9fafdf1edac89d3d2a6fe8354479",
+    "url": "/li-berty/index.html"
+  },
+  {
+    "revision": "a7847cfd9e5af76289328e7384a46270",
+    "url": "/li-berty/manifest.json"
+  },
+  {
+    "revision": "abcebb9691cdc26558975414a539561d",
+    "url": "/li-berty/robots.txt"
+  },
+  {
+    "revision": "a5f089ea8657af08026e",
+    "url": "/li-berty/static/css/app.ec89d167.css"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/li-berty/static/css/app.min.css"
+  },
+  {
+    "revision": "ffeda8d92e02b07a9851",
+    "url": "/li-berty/static/css/chunk-20c75f00.8989ecda.css"
+  },
+  {
+    "revision": "39a13f91247671dc83f0",
+    "url": "/li-berty/static/css/chunk-9a7a3f7e.8989ecda.css"
+  },
+  {
+    "revision": "f8c29ab039ed39345099",
+    "url": "/li-berty/static/css/chunk-f60b0500.8989ecda.css"
+  },
+  {
+    "revision": "e0ce93b55540e67be600",
+    "url": "/li-berty/static/css/chunk-vendors.ce38b027.css"
+  },
+  {
+    "revision": "ba4ed8fe2edc6af2190ace67f2daafb4",
+    "url": "/li-berty/static/css/colors.css"
+  },
+  {
+    "revision": "68b329da9893e34099c7d8ad5cb9c940",
+    "url": "/li-berty/static/css/fonts.css"
+  },
+  {
+    "revision": "b840618aefa4aedd792b875f58d3cdc2",
+    "url": "/li-berty/static/css/main.css"
+  },
+  {
+    "revision": "92aef81bacdbeedbae4e0e7e209eedf8",
+    "url": "/li-berty/static/fonts/fa-brands-400.92aef81b.woff"
+  },
+  {
+    "revision": "bcea4ab4780b632009ed0ae234fed3c4",
+    "url": "/li-berty/static/fonts/fa-brands-400.bcea4ab4.ttf"
+  },
+  {
+    "revision": "f37f3d6db6c06d870508a8a85e77c5db",
+    "url": "/li-berty/static/fonts/fa-brands-400.f37f3d6d.woff2"
+  },
+  {
+    "revision": "2ca27783cb7b54fda89dca12c29c1f2d",
+    "url": "/li-berty/static/fonts/fa-duotone-900.2ca27783.ttf"
+  },
+  {
+    "revision": "d61bac7c0ff5485d7b5206ecbc759023",
+    "url": "/li-berty/static/fonts/fa-duotone-900.d61bac7c.woff"
+  },
+  {
+    "revision": "ec4c036c4450f05b2f13dbbe8bb644b4",
+    "url": "/li-berty/static/fonts/fa-duotone-900.ec4c036c.woff2"
+  },
+  {
+    "revision": "56469d9f621c7d87c73c31ef4bb1bad0",
+    "url": "/li-berty/static/fonts/fa-light-300.56469d9f.ttf"
+  },
+  {
+    "revision": "edf3174234e44d44a156cf5fd9c50b24",
+    "url": "/li-berty/static/fonts/fa-light-300.edf31742.woff2"
+  },
+  {
+    "revision": "fcac9514d89d60fc3e55448af48f5cd9",
+    "url": "/li-berty/static/fonts/fa-light-300.fcac9514.woff"
+  },
+  {
+    "revision": "8bfca5e717ce3b59c3fc3ca2c66217a4",
+    "url": "/li-berty/static/fonts/fa-regular-400.8bfca5e7.woff2"
+  },
+  {
+    "revision": "b20db44b7f90e078b86f796069f155d6",
+    "url": "/li-berty/static/fonts/fa-regular-400.b20db44b.ttf"
+  },
+  {
+    "revision": "c2bca97ea1649ef6e39bd9f872cd8e8e",
+    "url": "/li-berty/static/fonts/fa-regular-400.c2bca97e.woff"
+  },
+  {
+    "revision": "26623167aec96f742cf168c6f344150a",
+    "url": "/li-berty/static/fonts/fa-solid-900.26623167.woff2"
+  },
+  {
+    "revision": "b904f5cfacff1c9c3e5c9aaad62a494d",
+    "url": "/li-berty/static/fonts/fa-solid-900.b904f5cf.ttf"
+  },
+  {
+    "revision": "e68a62d29893eeaf74a6493a31b94010",
+    "url": "/li-berty/static/fonts/fa-solid-900.e68a62d2.woff"
+  },
+  {
+    "revision": "b6a6ab8b2c29a14eb23627810dab43f8",
+    "url": "/li-berty/static/fonts/fa-thin-100.b6a6ab8b.woff2"
+  },
+  {
+    "revision": "c58def19127400136a99556f82649367",
+    "url": "/li-berty/static/fonts/fa-thin-100.c58def19.woff"
+  },
+  {
+    "revision": "da0900e6980dbd15e68ecb799da2b4ff",
+    "url": "/li-berty/static/fonts/fa-thin-100.da0900e6.ttf"
+  },
+  {
+    "revision": "54b0b4e7de85711c3796882b2b19eb00",
+    "url": "/li-berty/static/fonts/la-brands-400.54b0b4e7.woff2"
+  },
+  {
+    "revision": "a38ca9f0501109549cb659c1fe9ade65",
+    "url": "/li-berty/static/fonts/la-brands-400.a38ca9f0.eot"
+  },
+  {
+    "revision": "bbf83f8bb1039cd860051299d64ebcfd",
+    "url": "/li-berty/static/fonts/la-brands-400.bbf83f8b.ttf"
+  },
+  {
+    "revision": "fb598c9ccecd5fa1c6c769d0be60973b",
+    "url": "/li-berty/static/fonts/la-brands-400.fb598c9c.woff"
+  },
+  {
+    "revision": "2746742c09b070f74bd7d555e6b959fa",
+    "url": "/li-berty/static/fonts/la-regular-400.2746742c.eot"
+  },
+  {
+    "revision": "338f6f873b90c8045204f8ac52408166",
+    "url": "/li-berty/static/fonts/la-regular-400.338f6f87.woff"
+  },
+  {
+    "revision": "87dab6ff12ea107dafe1d52ec19c2ed8",
+    "url": "/li-berty/static/fonts/la-regular-400.87dab6ff.ttf"
+  },
+  {
+    "revision": "88d9d9416c58bde56378dc4439e3a144",
+    "url": "/li-berty/static/fonts/la-regular-400.88d9d941.woff2"
+  },
+  {
+    "revision": "36fc297902c9a2e857858baa6ac25f2c",
+    "url": "/li-berty/static/fonts/la-solid-900.36fc2979.woff2"
+  },
+  {
+    "revision": "87292218024ee1cab93406e228a0b7dd",
+    "url": "/li-berty/static/fonts/la-solid-900.87292218.woff"
+  },
+  {
+    "revision": "8c65fd3e9b53a609735fd6335fd05841",
+    "url": "/li-berty/static/fonts/la-solid-900.8c65fd3e.eot"
+  },
+  {
+    "revision": "bb49393b04bbf312a6cd055a051121d3",
+    "url": "/li-berty/static/fonts/la-solid-900.bb49393b.ttf"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Bold.eot"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Bold.ttf"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Bold.woff"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Bold.woff2"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Light.eot"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Light.ttf"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Light.woff"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Light.woff2"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Medium.eot"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Medium.ttf"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Medium.woff"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Medium.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Regular.eot"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Regular.ttf"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Regular.woff"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Regular.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Thin.eot"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Thin.ttf"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Thin.woff"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/li-berty/static/fonts/roboto/Roboto-Thin.woff2"
+  },
+  {
+    "revision": "270f72df47e036d9cfa11f91f0a94a60",
+    "url": "/li-berty/static/img/arrow_left.270f72df.svg"
+  },
+  {
+    "revision": "d9ccc77c8240ad0dc2724cf333c0479e",
+    "url": "/li-berty/static/img/arrow_right.d9ccc77c.svg"
+  },
+  {
+    "revision": "9bf634b4686212f68cd3117fa2086f0c",
+    "url": "/li-berty/static/img/beauty.9bf634b4.jpg"
+  },
+  {
+    "revision": "84dc7e6d06bdd6b3a27272be3dfece60",
+    "url": "/li-berty/static/img/carousel.84dc7e6d.jpg"
+  },
+  {
+    "revision": "9769d0a6a42cf73df12bc63a7a994131",
+    "url": "/li-berty/static/img/la-brands-400.9769d0a6.svg"
+  },
+  {
+    "revision": "55c57a74937e6de260b67c62522f7ea1",
+    "url": "/li-berty/static/img/la-regular-400.55c57a74.svg"
+  },
+  {
+    "revision": "79be4e9fcffc796ec3b2cb9a1f46d39e",
+    "url": "/li-berty/static/img/la-solid-900.79be4e9f.svg"
+  },
+  {
+    "revision": "5ac4350fc496dc976e6e5b85f4342c35",
+    "url": "/li-berty/static/img/logo.5ac4350f.png"
+  },
+  {
+    "revision": "e83ed56a647b4ba5c6fa0c6c5cc78ca2",
+    "url": "/li-berty/static/img/restaurant.e83ed56a.jpg"
+  },
+  {
+    "revision": "d46444fa464310f6c94abf6fa29b12ff",
+    "url": "/li-berty/static/img/unplugged.d46444fa.jpg"
+  },
+  {
+    "revision": "a5f089ea8657af08026e",
+    "url": "/li-berty/static/js/app.53383220.js"
+  },
+  {
+    "revision": "100ca7869e30dd0850eb",
+    "url": "/li-berty/static/js/chunk-03b4e0d6.f1bfe49e.js"
+  },
+  {
+    "revision": "ffeda8d92e02b07a9851",
+    "url": "/li-berty/static/js/chunk-20c75f00.1a085b4e.js"
+  },
+  {
+    "revision": "c3b947fccd94a70a3c3e",
+    "url": "/li-berty/static/js/chunk-2d2086e3.cfeaa80f.js"
+  },
+  {
+    "revision": "aa0c888bfca202822d5b",
+    "url": "/li-berty/static/js/chunk-2d2138a9.50371a56.js"
+  },
+  {
+    "revision": "80920a2596f573e24354",
+    "url": "/li-berty/static/js/chunk-38f11b31.72c4d0ec.js"
+  },
+  {
+    "revision": "1e4b8e20ee48b100c8e2",
+    "url": "/li-berty/static/js/chunk-4cd97e49.68d0e4cb.js"
+  },
+  {
+    "revision": "a95bbeb61321c1ed01cd",
+    "url": "/li-berty/static/js/chunk-6f27c912.c8bec857.js"
+  },
+  {
+    "revision": "39a13f91247671dc83f0",
+    "url": "/li-berty/static/js/chunk-9a7a3f7e.21ca9fdf.js"
+  },
+  {
+    "revision": "f8c29ab039ed39345099",
+    "url": "/li-berty/static/js/chunk-f60b0500.c62b3a20.js"
+  },
+  {
+    "revision": "e0ce93b55540e67be600",
+    "url": "/li-berty/static/js/chunk-vendors.6cede5e1.js"
+  }
+]);
